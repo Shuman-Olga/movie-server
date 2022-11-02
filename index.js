7c9027f5f0762ccc29fed7a81c77db5b53cb8710
Shuman-Olga <78848580+Shuman-Olga@users.noreply.github.com>
2022-11-03 01:05:52 +0700

    update env

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/api/movies/:id', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
