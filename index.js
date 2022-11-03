c977acc21b0e6a8dde432b879ac97d4f34caf82b
Shuman-Olga <78848580+Shuman-Olga@users.noreply.github.com>
2022-11-03 19:36:03 +0700

    cros

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express'),
   cors = require('cors'),
   dotenv = require('dotenv');
 dotenv.config();
-
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
